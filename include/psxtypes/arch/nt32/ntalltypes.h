a0d1666a327c3372f564f592e5c0854af13e3a51
midipix <writeonce@midipix.org>
2016-07-01 02:17:09 -0400

    added va_start, va_end, va_arg, and va_copy.

--- include/psxtypes/arch/nt32/ntalltypes.h
+++ include/psxtypes/arch/nt32/ntalltypes.h
@@ -178,3 +178,8 @@ typedef double			double_t;
 #endif
 
 typedef __CUSTOM_OR_BUILTIN_va_list__ va_list;
+
+#define va_start(v,l)	__builtin_va_start(v,l)
+#define va_end(v)	__builtin_va_end(v)
+#define va_arg(v,l)	__builtin_va_arg(v,l)
+#define va_copy(d,s)	__builtin_va_copy(d,s)
