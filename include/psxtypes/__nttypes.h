ecc2ff706522461325e597be72f52bfdee818e5a
midipix <writeonce@midipix.org>
2016-12-13 05:35:15 +0000

    perform the matching toolchain sanity check as the very final step.

--- include/psxtypes/__nttypes.h
+++ include/psxtypes/__nttypes.h
@@ -1,21 +1,3 @@
-/* verify matching compiler */
-#if defined(__NT32)
-
-typedef char __sanity[sizeof(int) - sizeof(void *) + 1];
-
-#elif defined(__NT64)
-
-#ifndef _NT64_SANITY_CHECKED
-#define _NT64_SANITY_CHECKED
-
-struct	__sanity_struct {int i; void * p;};
-typedef char __sanity[sizeof(struct __sanity_struct) - 2*sizeof(long long) + 1];
-
-#endif
-
-#endif
-
-
 /*compiler directives: unified syntax */
 #if defined(__GNUC__)
 #include "compiler/__nt_compiler_gcc.h"
@@ -30,5 +12,20 @@ typedef char __sanity[sizeof(struct __sanity_struct) - 2*sizeof(long long) + 1];
 #include "arch/nt32/ntalltypes.h"
 #elif defined(__NT64)
 #include "arch/nt64/ntalltypes.h"
+#endif
+#endif
+
+
+/* verify matching compiler */
+#if defined(__NT32)
+typedef char __sanity[sizeof(int) - sizeof(void *) + 1];
+#endif
+
+
+#if defined(__NT64)
+#ifndef _NT64_SANITY_CHECKED
+#define _NT64_SANITY_CHECKED
+struct	__sanity_struct {int i; void * p;};
+typedef char __sanity[sizeof(struct __sanity_struct) - 2*sizeof(long long) + 1];
 #endif
 #endif
